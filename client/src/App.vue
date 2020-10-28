f6bbb8f2b7c4fca38cb6957da4ffcb6111b4cc34
CatAnderson <thecatanderson100@gmail.com>
2020-10-28 14:07:24 +0000

    starting front end work

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<ul>
+  <li v-for="guest in guests" :guest="guest"> {{guest.name}}</li>
+</ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import GuestService from './services/GuestService.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  
+  data(){
+    return{
+      guests: []
   }
+  },
+
+  mounted(){
+    this.fetchGuests();
+  },
+
+  methods: {
+    fetchGuests(){
+      GuestService.getGuests()
+      .then((guests) => (this.guests = guests));
+    }
+  }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/GuestGrid.vue
@@ -0,0 +1,22 @@
+<template lang='html'>
+  <section>
+      <guest v-for="(guest, index) in guests" :key='index' :guest="guest" />
+  </section>
+</template>
+
+<script>
+import Guest from './Guest.vue'
+import GuestService from '@/services/GuestService.js'
+
+export default {
+    name: 'guest-grid',
+    components:{
+        guest: Guest
+    },
+    props: ['guests']
+}
+</script>
+
+<style lang='css' scoped>
+
+</style>
